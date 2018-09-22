ce1aeb6021854265fcb3dbba2807f9c101f794ef
fverna <43486409+fverna@users.noreply.github.com>
2018-09-22 11:42:58 -0600

    C# practice in App1 by fverna.

--- Unity/App1/Program.cs
+++ Unity/App1/Program.cs
@@ -6,8 +6,46 @@ namespace App1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a setup for a simple test app.");
+            Console.WriteLine("This is a simple test app for learning C#.\n");
+            while(!GetName());
+            while(!GetAge());
             Console.ReadKey();
+        }
+
+        private static bool GetName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("You input '{0}' as your name, is this correct? [Y/n]", name);
+            char check = Convert.ToChar(Console.ReadLine());
+            if ((check == 'Y') || (check == 'y'))
+            {
+                Console.WriteLine("Perfect. It's great to meet you {0}!\n", name);
+                return true;
+            }
+            else if ((check == 'N') || (check == 'n'))
+                Console.WriteLine("Oops, something went wrong then. Let's try again.\n");
+            else
+                Console.WriteLine("Sorry, I don't recognize that response. Let's try again.\n");
+            return false;
+        }
+
+        private static bool GetAge()
+        {
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You input '{0}' as your age, is this correct? [Y/n]", age);
+            char check = Convert.ToChar(Console.ReadLine());
+            if ((check == 'Y') || (check == 'y'))
+            {
+                Console.WriteLine("Ripe old age of {0}, huh? Huehuehue.\n", age);
+                return true;
+            }
+            else if ((check == 'N') || (check == 'n'))
+                Console.WriteLine("Oops, something went wrong then. Let's try again.\n");
+            else
+                Console.WriteLine("Sorry, I don't recognize that response. Let's try again.\n");
+            return false;
         }
     }
 }
