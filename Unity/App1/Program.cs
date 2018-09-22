fb033104c85636ca96f1b100c1170992a0d3de55
fverna <43486409+fverna@users.noreply.github.com>
2018-09-21 23:32:30 -0600

    More VS experimenting.

---
+++ Unity/App1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a setup for a simple test app.");
+            Console.ReadKey();
+        }
+    }
+}
