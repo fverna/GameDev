110d9e8a19b7aefe6628e025ec89a98e48bf323b
fverna <43486409+fverna@users.noreply.github.com>
2018-09-21 22:54:27 -0600

    VS Stuff.

---
+++ Unity/hello_world.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorldApplication
+{
+	class HelloWorld
+    {
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world, let's do some game development!\n");
+            Console.ReadKey();
+        }
+    }
+}
